e30637f6f17e10089a833eff65e4c1e318e31cdc
CBurg-College <cburgmicro@outlook.com>
2025-09-13 11:01:33 +0200

    main.ts bijwerken

--- main.ts
+++ main.ts
@@ -326,6 +326,272 @@ namespace Wave {
     }
 }
 
+//////////////////////
+//##################//
+//##              ##//
+//##  gamepad.ts  ##//
+//##              ##//
+//##################//
+//////////////////////
+
+//% color="#C4C80E" icon="\uf11b"
+//% block="Gamepad"
+//% block.loc.nl="Gamepad"
+namespace Gamepad {
+
+    export enum Joystick {
+        //% block="none"
+        //% block.loc.nl="geen"
+        None,
+        //% block="up"
+        //% block.loc.nl="omhoog"
+        Up,
+        //% block="right up"
+        //% block.loc.nl="rechts omhoog"
+        UpRight,
+        //% block="right"
+        //% block.loc.nl="rechts"
+        Right,
+        //% block="right down"
+        //% block.loc.nl="rechts omlaag"
+        DownRight,
+        //% block="down"
+        //% block.loc.nl="omlaag"
+        Down,
+        //% block="left down"
+        //% block.loc.nl="links omlaag"
+        DownLeft,
+        //% block="left"
+        //% block.loc.nl="links"
+        Left,
+        //% block="left up"
+        //% block.loc.nl="links omhoog"
+        UpLeft
+    }
+
+    let JSANGLE = 0
+    let JSPOWER = 0
+    let JSDIR = Joystick.None
+
+    export enum Power {
+        //% block="Full power"
+        //% block.loc.nl="volle kracht"
+        Full,
+        //% block="Half full power"
+        //% block.loc.nl="halfvolle kracht"
+        HalfFull,
+        //% block="Half power"
+        //% block.loc.nl="halve kracht"
+        Half,
+        //% block="Low power"
+        //% block.loc.nl="weinig kracht"
+        HalfLow,
+        //% block="without power"
+        //% block.loc.nl="zonder kracht"
+        Low
+    }
+    export enum Button {
+        //% block="up"
+        //% block.loc.nl="omhoog"
+        Up, //P12
+        //% block="down"
+        //% block.loc.nl="omlaag"
+        Down, //P15 
+        //% block="left"
+        //% block.loc.nl="links"
+        Left, //P13
+        //% block="right"
+        //% block.loc.nl="rechts"
+        Right //P14
+    }
+
+    let BUTTONMAX = 4
+
+    let PRESSED1 = false
+    let PRESSED2 = false
+    let PRESSED3 = false
+    let PRESSED4 = false
+
+    let joystickXHandler: handler
+    let joystickNHandler: handler
+    let joystickNEHandler: handler
+    let joystickEHandler: handler
+    let joystickSEHandler: handler
+    let joystickSHandler: handler
+    let joystickSWHandler: handler
+    let joystickWHandler: handler
+    let joystickNWHandler: handler
+
+    let pressed1Handler: handler
+    let pressed2Handler: handler
+    let pressed3Handler: handler
+    let pressed4Handler: handler
+
+    let released1Handler: handler
+    let released2Handler: handler
+    let released3Handler: handler
+    let released4Handler: handler
+
+    function handleJoystick(value: number) {
+        JSPOWER = Math.floor(value / 1000)
+        JSANGLE = value - JSPOWER * 1000
+        let dir: Joystick
+
+        if (JSPOWER) {
+            if (JSANGLE > 338 || JSANGLE < 23) dir = Joystick.Up
+            if (JSANGLE > 23 && JSANGLE < 68) dir = Joystick.UpRight
+            if (JSANGLE > 68 && JSANGLE < 113) dir = Joystick.Right
+            if (JSANGLE > 113 && JSANGLE < 158) dir = Joystick.DownRight
+            if (JSANGLE > 158 && JSANGLE < 203) dir = Joystick.Down
+            if (JSANGLE > 203 && JSANGLE < 248) dir = Joystick.DownLeft
+            if (JSANGLE > 248 && JSANGLE < 293) dir = Joystick.Left
+            if (JSANGLE > 293 && JSANGLE < 338) dir = Joystick.UpLeft
+        }
+        else
+            dir = Joystick.None
+
+        if (dir == JSDIR) return;
+        JSDIR = dir
+
+        if ((JSDIR == Joystick.None) && joystickXHandler)
+            joystickXHandler()
+        if ((JSDIR == Joystick.Up) && joystickNHandler)
+            joystickNHandler()
+        if ((JSDIR == Joystick.UpRight) && joystickNEHandler)
+            joystickNEHandler()
+        if ((JSDIR == Joystick.Right) && joystickEHandler)
+            joystickEHandler()
+        if ((JSDIR == Joystick.DownRight) && joystickSEHandler)
+            joystickSEHandler()
+        if ((JSDIR == Joystick.Down) && joystickSHandler)
+            joystickSHandler()
+        if ((JSDIR == Joystick.DownLeft) && joystickSWHandler)
+            joystickSWHandler()
+        if ((JSDIR == Joystick.Left) && joystickWHandler)
+            joystickWHandler()
+        if ((JSDIR == Joystick.UpLeft) && joystickNWHandler)
+            joystickNWHandler()
+    }
+
+    function handlePressed(button: Button) {
+        switch (button) {
+            case Button.Up: PRESSED1 = true; if (pressed1Handler) pressed1Handler(); break;
+            case Button.Down: PRESSED2 = true; if (pressed2Handler) pressed2Handler(); break;
+            case Button.Left: PRESSED3 = true; if (pressed3Handler) pressed3Handler(); break;
+            case Button.Right: PRESSED4 = true; if (pressed4Handler) pressed4Handler(); break;
+        }
+    }
+
+    function handleReleased(button: Button) {
+        switch (button) {
+            case Button.Up: PRESSED1 = false; if (released1Handler) released1Handler(); break;
+            case Button.Down: PRESSED2 = false; if (released2Handler) released2Handler(); break;
+            case Button.Left: PRESSED3 = false; if (released3Handler) released3Handler(); break;
+            case Button.Right: PRESSED4 = false; if (released4Handler) released4Handler(); break;
+        }
+    }
+
+    radio.onReceivedNumber(function (value: number) {
+        if (value >= 1000)
+            handleJoystick(value - 1000)
+        else {
+            if (value >= BUTTONMAX)
+                handleReleased(value - BUTTONMAX)
+            else
+                handlePressed(value)
+        }
+    })
+
+    //% block="%button is up"
+    //% block.loc.nl="%button is losgelaten"
+    export function isReleased(button: Button): boolean {
+        switch (button) {
+            case Button.Up: return !PRESSED1;
+            case Button.Down: return !PRESSED2;
+            case Button.Left: return !PRESSED3;
+            case Button.Right: return !PRESSED4;
+        }
+        return false;
+    }
+
+    //% block="%button is down"
+    //% block.loc.nl="%button is ingedrukt"
+    export function isPressed(button: Button): boolean {
+        switch (button) {
+            case Button.Up: return PRESSED1;
+            case Button.Down: return PRESSED2;
+            case Button.Left: return PRESSED3;
+            case Button.Right: return PRESSED4;
+        }
+        return false;
+    }
+
+    //% block="joystick-power"
+    //% block.loc.nl="joystick-kracht"
+    export function readPower(): Power {
+        let pwr: Power
+        if (JSPOWER > 80) pwr = Power.Full
+        else
+            if (JSPOWER > 60) pwr = Power.HalfFull
+            else
+                if (JSPOWER > 40) pwr = Power.Half
+                else
+                    if (JSPOWER > 20) pwr = Power.HalfLow
+                    else
+                        pwr = Power.Low
+        return pwr
+    }
+
+    //% block="joystick Joystick"
+    //% block.loc.nl="joystick-richting"
+    export function readJoystick(): Joystick {
+        return JSDIR
+    }
+
+    //% color="#FFC000"
+    //% block="when %button is released"
+    //% block.loc.nl="wanneer %button wordt losgelaten"
+    export function onReleased(button: Button, code: () => void): void {
+        switch (button) {
+            case Button.Up: released1Handler = code; break;
+            case Button.Down: released2Handler = code; break;
+            case Button.Left: released3Handler = code; break;
+            case Button.Right: released4Handler = code; break;
+        }
+    }
+
+    //% color="#FFC000"
+    //% block="when %button is pressed"
+    //% block.loc.nl="wanneer %button wordt ingedrukt"
+    export function onPressed(button: Button, code: () => void): void {
+        switch (button) {
+            case Button.Up: pressed1Handler = code; break;
+            case Button.Down: pressed2Handler = code; break;
+            case Button.Left: pressed3Handler = code; break;
+            case Button.Right: pressed4Handler = code; break;
+        }
+    }
+
+    //% color="#FFC000"
+    //% block="when the joystick Joystick is %dir"
+    //% block.loc.nl="wanneer de joystick richting %dir is"
+    export function onJoystick(dir: Joystick, code: () => void): void {
+        switch (dir) {
+            case Joystick.None: joystickXHandler = code; break;
+            case Joystick.Up: joystickNHandler = code; break;
+            case Joystick.UpRight: joystickNEHandler = code; break;
+            case Joystick.Right: joystickEHandler = code; break;
+            case Joystick.DownRight: joystickSEHandler = code; break;
+            case Joystick.Down: joystickSHandler = code; break;
+            case Joystick.DownLeft: joystickSWHandler = code; break;
+            case Joystick.Left: joystickWHandler = code; break;
+            case Joystick.UpLeft: joystickNWHandler = code; break;
+        }
+    }
+
+}
+
+
 ////////////////////////
 //####################//
 //##                ##//
